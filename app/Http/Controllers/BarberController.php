4ec66125dc031fb056bd9a225e71310263921b31
ArnoczkiAttila <77162976+ArnoczkiAttila@users.noreply.github.com>
2025-02-28 08:41:48 +0100

    asd

---
+++ app/Http/Controllers/BarberController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Barber;
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+
+class BarberController extends Controller
+{
+    public function index() {
+        $barbers = Barber::all();
+        return response()->json($barbers,200,options:JSON_UNESCAPED_UNICODE);
+    }
+    public function store(Request $request) {
+        try {
+            $request->validate([
+                'barber_name'=>['required','string','max:255']
+            ],[
+                'required'=>'A :attribute mező kitöltése kötelező!',
+                'string'=>'A :attribute mezőnek string tipusúnak kell lennie!',
+                'max'=>'A :attribute mezőnek a hossza nem lehet több 255-nél'
+            ],[
+                'barber_name'=>'név'
+            ]);
+        } catch (ValidationException $e) {
+            return response()->json(['success'=>false,'message'=>$e->errors()],200,options:JSON_UNESCAPED_UNICODE);
+            
+        }
+        Barber::create($request->all());
+        return response()->json(['success'=>true,'message'=>'Sikeres léterhozás!'],200,options:JSON_UNESCAPED_UNICODE);
+    }
+    public function destroy(Request $request) {
+        
+        Barber::find($request->id)->delete();
+        return response()->json(['success'=>true,'message'=>'Sikeres törlés!'],200,options:JSON_UNESCAPED_UNICODE);
+    }
+}
---
+++ app/Models/Appointment.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Appointment extends Model
+{
+    protected $fillable = [
+        'name',
+        'barber_id',
+        'appointment'
+    ];
+    public function barber() {
+        return $this->belongsTo(Barber::class);
+    }
+}
